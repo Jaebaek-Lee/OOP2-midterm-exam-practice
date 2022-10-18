4fb8c1db73f6ce060abcd2844704d0b3c5dca8f6
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-10-19 02:37:31 +0900

    얕은 복사 생성자

---
+++ 중간 연습.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+/* 얕은 복사생성자
+class Student {
+private:
+	int id;
+	string name;
+public:
+	Student(int id, string name) {
+		this->id = id;
+		this->name = name;
+		cout << "parameter constructor: " << this->id << " " << this->name << endl;
+	}
+	Student(const Student& origin) { //레퍼런스를 사용하지 않으면 무한복사... const: 함수 내에서 변경되지 않고 값만 사용함을 명시한다.
+		this->name = origin.name;
+		this->id = origin.id;
+		cout << "copy constructor: " << this->id << " " << this->name << endl;
+	}
+};
+
+int main() {
+	Student s1{ 1, "pby" };
+	Student s2{ s1 };
+}
+*/
